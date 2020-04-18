46a6ec2d59e6981106a400a51b2b54217251c05d
adler-sudo <52939328+adler-sudo@users.noreply.github.com>
2020-04-18 09:14:38 -0700

    Update app.py
    made quite a few changes. this information provides much better insight into the changes in wildfire occurrences and causes in Oregon since 1960.

--- app.py
+++ app.py
@@ -11,8 +11,12 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+
 # import plotly
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly
+
 
 # import data handling packages
 import pandas as pd
@@ -20,253 +24,131 @@ from sqlalchemy import (create_engine,
                         select,
                         Table,
                         MetaData)
-import numpy as np
 
-# convert database to dataframe
+
+# convert fire database to dataframe
 engine = create_engine('sqlite:///fire.db')
 conn = engine.connect()
 metadata = MetaData()
+
 fires = Table('fires', metadata, autoload=True, autoload_with=engine)
+geo = Table('geo', metadata, autoload=True, autoload_with=engine)
+
 query = select([fires])
+queryGeo = select([geo])
+
 records = conn.execute(query).fetchall()
+recordsGeo = conn.execute(queryGeo).fetchall()
+
 df = pd.DataFrame(records, columns=fires.columns.keys())
+dfGeo = pd.DataFrame(recordsGeo, columns=geo.columns.keys())
+
 df.drop(columns=['index'], inplace=True)
+dfGeo.drop(columns=['index'], inplace=True)
+dfGeo.dropna(subset=['total_acres'], inplace=True)
+
 df[df.general_cause.isnull()] = 'Miscellaneous'
 df = df[df.fire_year != 'Miscellaneous']
 df['fire_year'] = df.fire_year.astype(int)
-
-# prepare data for overview graph
-x = df.fire_year.unique()
-y = df.groupby('fire_year')['total_acres'].sum()
 
 # initiate app
 app = dash.Dash(__name__)
 
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': '#483C32',
+    'text': '#B5EAAA',
 }
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Oregon Wildfire Dashboard',
-        style={
-            'textAlign': 'left',
-            'color': colors['text']
-        }
-    ),
-    
-    # overview graph
-    html.Div(children='A page for Oregon wildfire data visualization.', style={
-        'textAlign': 'left',
-        'color': colors['text']
-    }),
+# prepare components for stacked bar
+stacked_df = df
+causes = stacked_df.general_cause.unique()
+fire_years = stacked_df.fire_year.unique()
+graphs = []
+plotColors = plotly.colors.qualitative.Alphabet
 
-    dcc.Graph(
-        id='total_acres_burned',
-        figure={
-            'data': [
-                {'x': x,
-                 'y': y,
-                 'type': 'bar'}
-                
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                },
-                'title': 'Total Acres Burned by Year',
-                'xaxis':{
-                    'title':'Year'
-                },
-                'yaxis':{
-                    'title':'Acres Burned'
-                }
-            }
-        }
-    ),
+# create the total acres burned graph by general cause
+for i in range(len(causes)):
+    bar = go.Bar(name='{}'.format(causes[i]), x=list(stacked_df.fire_year.unique()),
+                     y=list(stacked_df[stacked_df.general_cause == causes[i]].groupby('fire_year')['total_acres'].sum()),
+                     marker_color=plotColors[i]
+                     )
+    graphs.append(bar)
     
-    # general cause graph with range slider  
-    html.Div(id='dynamicBarSet',
-             className='dynamicBarSet',
-             children=[
-        
-    dcc.Dropdown(
-        id='cause_dropdown',
-        options=[{'label': i, 'value': i} for i in df.general_cause.unique()],
-        value='Lightning'
-    ),
-               
-    dcc.Graph(
-        id='dynamic_graph',
-    ),
-    
-    
-    dcc.RangeSlider(
-        id='my_range_slider',
-        min=min(df.fire_year),
-        max=max(df.fire_year),
-        step=1,
-        marks={
-            1961: '1961',
-            1970: '1970',
-            1980: '1980',
-            1990: '1990',
-            2000: '2000',
-            2010: '2010',
-            2019: '2019'
-        },
-        value=[2010, 2019]
-    ),
-            ]
-    ),
-    
-    # pie charts
-    html.Div(id='pieParent',
-             className='pieParent',
-             children=[
-    
-    # first pie chart
-    html.Div(id='divYear',
-             className='year',
-             children=[
-                dcc.Graph(
-                    id='piechartYear'
-                ),
-                
-                dcc.Dropdown(
-                    id='piechartInput',
-                    options=[{'label': i, 'value': i} for i in df.fire_year.unique()],
-                    value=2019
+fig = go.Figure(data=graphs)
+fig.update_layout(barmode='stack',
+                  title='Oregon Acres Burned by Cause by Year',
+                  xaxis={'title':'Year'},
+                  yaxis={'title':'Total Acres Burned'},
+                  plot_bgcolor='#C3FBD8',
+                  paper_bgcolor='#483C32',
+                  font={'color':'#B5EAAA'},
+                  )
+
+
+countGraphs = []
+
+# create the stacked count graph by general cause
+for i in range(len(causes)):
+    bar = go.Bar(name='{}'.format(causes[i]),
+                 x=list(stacked_df.fire_year.unique()),
+                 y=list(stacked_df[stacked_df.general_cause == causes[i]].groupby('fire_year')['general_cause'].count()),
+                 marker_color=plotColors[i]
+                 )
+    countGraphs.append(bar)
+
+countGraph = go.Figure(data=countGraphs)
+countGraph.update_layout(barmode='stack',
+                         title='Oregon Fire Count by Cause by Year',
+                         xaxis={'title':'Year'},
+                         yaxis={'title':'Total Count of Fires'},
+                         plot_bgcolor='black',
+                         paper_bgcolor='#483C32',
+                         font={'color':'#B5EAAA'},
+                         )
+
+
+
+# set app layout
+app.layout = html.Div(style={'backgroundColor': colors['background']},
+                      children=[
+                                html.H1(
+                                    children='Oregon Wildfire Dashboard',
+                                    style={
+                                        'textAlign': 'left',
+                                        'color': colors['text']
+                                    }
+                                ),
+                            
+                                # the main body
+                                html.Div(
+                                    id='description',
+                                    children='A page for Oregon wildfire data visualization.', style={
+                                    'textAlign': 'left',
+                                    'color': colors['text']
+                                }),
+                                
+                                html.Div(id='theGraphsDiv',
+                                         className='theGraphsClass',
+                                         children=[
+                                                html.Div(id='stackedDiv',
+                                                         children=[
+                                                             dcc.Graph(id='stackedGraph',
+                                                                       figure=fig
+                                                             )
+                                                        ]
+                                                ),
+                                                html.Div(id='countDiv',
+                                                         children=[
+                                                             dcc.Graph(id='countGraph',
+                                                                       figure=countGraph)
+                                                             ]
+                                                )
+                                            ]
+                                )
+                            
+                        ]
                 )
-            ]
-    ),
-    
-    # second pie chart
-    html.Div(id='divCounty',
-             className='county',
-             children=[
-                dcc.Graph(
-                    id='piechartCounty'
-                ),
-                
-                dcc.Dropdown(
-                    id='piechartCountyInput',
-                    options=[{'label': i, 'value': i} for i in df.fire_year.unique()],
-                    value=2019
-                )
-            ]
-    ),
 
-    # third pie chart
-    html.Div(id='divRedundant',
-             className='redundant',
-             children=[
-                dcc.Graph(
-                    id='piechartRedundant'
-                ),
-                dcc.Dropdown(
-                    id='piechartRedundantInput',
-                    options=[{'label': i, 'value': i} for i in df.general_cause.unique()],
-                    value='Smoking'
-                )
-              ]
-    )
-            ]
-    )
-])
-
-
-# app callback for dynamic bar graph
-@app.callback(
-    Output(component_id='dynamic_graph', component_property='figure'),
-    [Input(component_id='cause_dropdown', component_property='value'),
-     Input('my_range_slider', 'value')])
-def update_output_div(cause, year):
-    filtered_df = df[df.general_cause == cause]
-    
-    mask = (filtered_df.fire_year >= year[0]) & (filtered_df.fire_year <= year[1])
-    filtered_df = filtered_df.loc[mask]
-    
-    return {
-            'data': [{'x': filtered_df.fire_year.unique(),
-                 'y': filtered_df.groupby('fire_year')['total_acres'].sum(),
-                 'type':'bar'}
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                },
-                'xaxis':{'title': 'Year'
-                },
-                'yaxis':{'title': 'Total Acres Burned'
-                },
-                'title': 'Total Acres Burned by Year due to {}'.format(cause)
-            }
-    }
-       
-
-# app callback for piechart by cause
-@app.callback(
-    Output('piechartYear', 'figure'),
-    [Input('piechartInput', 'value')])
-def update_piechart(year):
-    piechart_df = df[df.fire_year == year]
-    
-    piechart = px.pie(
-                    data_frame=piechart_df,
-                    values='total_acres',
-                    names='general_cause',
-                    title='Oregon Wildfire Cause Distribution in {}'.format(year),
-                    color_discrete_sequence=px.colors.cyclical.IceFire,
-                    )
-    
-    piechart.update_traces(textposition='inside', textinfo='percent+label')
-    
-    return piechart
-
-
-# app callback for pie chart by county
-@app.callback(
-    Output('piechartCounty', 'figure'),
-    [Input('piechartCountyInput', 'value')])
-def update_piechartCounty(year):
-    piechart_df = df[df.fire_year == year]
-    
-    piechartCounty = px.pie(
-                    data_frame=piechart_df,
-                    values='total_acres',
-                    names='county',
-                    title='Oregon Wildfire County Distribution in {}'.format(year),
-                    color_discrete_sequence=px.colors.sequential.Viridis,
-                    )
-    
-    piechartCounty.update_traces(textposition='inside', textinfo='percent+label')
-    
-    return piechartCounty
-    
-# app callback for pie chart by county
-@app.callback(
-    Output('piechartRedundant', 'figure'),
-    [Input('piechartRedundantInput', 'value')])
-def update_piechartCause(cause):
-    piechart_df = df[df.general_cause == cause]
-    
-    piechartYear = px.pie(
-                    data_frame=piechart_df,
-                    values='total_acres',
-                    names='fire_year',
-                    title='Year Breakdown by {}'.format(cause),
-                    color_discrete_sequence=px.colors.sequential.Magma,
-                    )
-    
-    piechartYear.update_traces(textposition='inside', textinfo='percent+label')
-    
-    return piechartYear
 
 
 if __name__ == '__main__':
