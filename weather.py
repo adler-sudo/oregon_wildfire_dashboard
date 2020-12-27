2c3aa7342bf38290b437fc65a25b2cf594228273
adler-sudo <52939328+adler-sudo@users.noreply.github.com>
2020-12-27 15:02:08 -0800

    added ability to filter by location

--- weather.py
+++ weather.py
@@ -7,13 +7,20 @@ import dash_html_components as html
 import plotly.express as px
 from shapely.geometry import Polygon
 import requests
+from dash.dependencies import Output, Input
+import numpy as np
+import sqlite3
 
 
-
+# TODO: need to go in and switch this initial df/fig to come from weatherData
 # convert fire database to dataframe
-engine = create_engine('sqlite:///practiceWeather.db')
-conn = engine.connect()
-metadata = MetaData()
+# sqlalcehmy method
+# engine = create_engine('sqlite:///practiceWeather.db')
+# conn = engine.connect()
+# metadata = MetaData()
+
+# sqlite3 method
+con = sqlite3.connect("practiceWeather.db")
 
 # select weather table
 # observations = select([Table('observations', metadata, autoload=True, autoload_with=engine)])
@@ -26,6 +33,12 @@ metadata = MetaData()
 # dfGeo.dropna(subset=['total_acres'], inplace=True)
 
 # select locations table
+
+# TODO: need to switch this to come from weather data and pick a specific day
+# extract locations list
+query = 'SELECT NAME FROM practice ORDER BY NAME'
+locations = con.execute(query).fetchall()
+locations = [l for l, in locations]
 
 
 # state polygon preparation
@@ -40,12 +53,13 @@ fig_poly = px.line(x=x, y=y, color_discrete_sequence=px.colors.qualitative.G10)
 
 # right now just set up to use single day from practice data since
 # primary database is so large
-df = pd.read_sql('SELECT * FROM practice', con=conn)
+df = pd.read_sql('SELECT * FROM practice', con=con)
 fig = px.scatter(df,
                  'LONGITUDE',
                  'LATITUDE',
-                 color='PRCP',
-                 size='PRCP',
+                 color='SNOW',
+                 size='ELEVATION',
+                 hover_name='CITY',
                  height=1000,
                  width=1600)
 
@@ -63,14 +77,62 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(
     children=[
+        dcc.Dropdown(
+            id='location-dropdown',
+            options=[
+                {'label': l, 'value': l} for l in locations
+            ],
+            multi=True,
+            value=np.array(locations),
+            style={
+                'height': '50vh',
+                'overflow-y': 'scroll'
+            }
+        ),
         dcc.Graph(
+            id='weather-map',
             figure=fig
         )
     ]
 )
 
+# location-specific map
+# will want this to eventually include locations within a given region
+@app.callback(
+    Output('weather-map', 'figure'),
+    Input('location-dropdown', 'value'))
+def update_map(location):
 
 
+    # TODO: come back to this later to make call within callback statement after thread location is ignored
+    # locs = ', '.join('"' + l + '"' for l in location)
+    # query = 'SELECT o.SNOW, l.LONGITUDE, l.LATITUDE, l.ELEVATION, l.CITY FROM observations AS o JOIN locations AS l WHERE l.CITY IN (%s)' % locs
+    # filtered_df = pd.read_sql(query, con=conn)
+
+    filtered_df = df[df.CITY.isin(location)]
+
+    fig = px.scatter(filtered_df,
+                     'LONGITUDE',
+                     'LATITUDE',
+                     color='SNOW',
+                     size='ELEVATION',
+                     hover_name='CITY',
+                     height=1000,
+                     width=1600)
+
+    # add trace of oregon
+    fig.add_trace(fig_poly.data[0])
+
+    # create a consistent view of the entire state of oregon
+    fig.update_layout(yaxis_range=[41.75, 46.5],
+                      xaxis_range=[-124.75, -116.25])
+
+    return fig
+
+# TODO: add ability to select date to filter map
+
+
+# run app
 if __name__ == '__main__':
     app.run_server(debug=True)
 
