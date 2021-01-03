e3b1364f2be892fa2d74961b7cdc0d3f78f8fdf2
adler-sudo <52939328+adler-sudo@users.noreply.github.com>
2021-01-03 15:30:16 -0800

    refining functions

--- prcp_funcs.py
+++ prcp_funcs.py
@@ -1,15 +1,34 @@
 import datetime
 
 
-def precipitation_average(df, interest_date, day_range=30):
+def weather_average(df, interest_variable, interest_date, day_range=30):
     """
     Parameters:
         df: dataframe of interest
+        interest_variable: weather variable of interest
         interest_date: date to look back from
         day_range: # of days to look back
 
     returns the average precipitation over a specified period of time looking back from a specified date
     """
-    average = df.loc[(df.DATE > interest_date - datetime.timedelta(day_range)) & (df.DATE <= interest_date)]['PRCP'].mean()
+    average = df.loc[(df.DATE > interest_date - datetime.timedelta(day_range)) & (df.DATE <= interest_date)][
+        interest_variable].mean()
 
     return average
+
+
+def fire_count(df, interest_date, day_range=30):
+    """
+    Parameters:
+        df: dataframe of interest. dataframe comes from geo table in fire.db and must already be
+            parsed for the area of interest
+        interest_date: date to look forward from
+        day_range: # of days to look forward
+
+    returns a count of the number of fires looking forward a specified # of days from a specified date
+    """
+    fires = len(df.loc[(df.report_date > interest_date) & (df.report_date < interest_date + datetime.timedelta(day_range))])
+
+    return fires
+
+
