e000c08a6d4c09532e61ae8d1565510bf53d14bc
adler-sudo <52939328+adler-sudo@users.noreply.github.com>
2021-01-07 17:26:45 -0800

    initiated fire_calcs submodule and separated weather and fire calcs

--- calcs/weather_calcs.py
+++ calcs/weather_calcs.py
@@ -19,23 +19,6 @@ def weather_average(df, interest_variable, interest_date, day_range=30):
     return average
 
 
-# TODO: move this to a different module
-# TODO: reduce the day range to 7?
-def fire_count(df, interest_date, day_range=7):
-    """
-    Parameters:
-        df: dataframe of interest. dataframe comes from geo table in fire.db and must already be
-            parsed for the area of interest
-        interest_date: date to look forward from
-        day_range: # of days to look forward
-
-    returns a count of the number of fires looking forward a specified # of days from a specified date
-    """
-    fires = len(df.loc[(df.report_date > interest_date) & (df.report_date < interest_date + datetime.timedelta(day_range))])
-
-    return fires
-
-
 def location_weather_df(location, analysis_type):
     """
     Parameters:
@@ -56,21 +39,4 @@ def location_weather_df(location, analysis_type):
     weather_df.reset_index(drop=True, inplace=True)
 
     return weather_df
-
-
-# TODO: will be able to be smarter and consolidate the fire count and fire df dataframes
-# TODO: move this to a different module
-def location_fire_df(latitude, longitude):
-    """
-    returns dataframe of fires within 0.5 degrees of latitude and longitude
-    """
-    fire_con = sqlite3.connect('../fire.db')
-
-    # make this a count query in the future
-    fire_query = 'SELECT * FROM geo WHERE latitude < ? + 0.5 AND latitude > ? - 0.5 AND longitude < ? + 0.5 AND longitude > ? - 0.5'
-
-    fire_df = pd.read_sql(fire_query, con=fire_con, params=[latitude, latitude, longitude, longitude], parse_dates=['report_date'])
-
-    return fire_df
-
 
